a58f89d88e2a1dc4c7bb5a4a4f48cd2210bf032a
Tomas Peranic <peranict@sheridancollege.ca>
2020-02-13 21:39:28 -0500

    Add project files.

---
+++ ShoppingListApp/DAL/ShoppingContext.cs
@@ -0,0 +1,27 @@
+﻿using ShoppingListApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingListApp.DAL
+{
+    public class ShoppingContext : DbContext
+    {
+        public ShoppingContext() : base("ShoppingContext")
+        {
+
+        }
+
+        public DbSet<ShoppingList> ShoppingLists { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        }
+
+    }
+}
---
+++ ShoppingListApp/DAL/ShoppingInitializer.cs
@@ -0,0 +1,25 @@
+﻿using ShoppingListApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingListApp.DAL
+{
+    public class ShoppingInitializer : System.Data.Entity. DropCreateDatabaseIfModelChanges<ShoppingContext>
+    {
+        protected override void Seed(ShoppingContext context)
+        {
+            var shopping = new List<ShoppingList>
+            {
+                new ShoppingList{ID=1, Username="Tomas", Password="Tomas123", Items="Banana, Apple, Pear"},
+                new ShoppingList{ID=2, Username="Viktor", Password="Viktor123", Items="Chocolate, Water, Ketchup"},
+                new ShoppingList{ID=3, Username="Dexter", Password="Dexter123", Items="Burger, Fries, Orange"},
+
+            };
+
+            shopping.ForEach(s => context.ShoppingLists.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
